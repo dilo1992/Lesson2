5f4c7de6894cf46490f6652eb89e57a5b95a0672
dilo1992 <dimonchikov1992@gmail.com>
2022-08-03 20:39:35 +0300

    Random and if-else

--- src/RandomAndNumber.java
+++ src/RandomAndNumber.java
@@ -2,14 +2,18 @@ import java.util.Random;
 
 public class RandomAndNumber {
     public static void main(String[] args) {
-        int randomValue=new Random().nextInt();
-        if (randomValue%2==0){
-            String typeNumber="even";
-            System.out.println("Number "+randomValue+" is "+typeNumber);
+        int randomValue=new Random().nextInt(12)+1;
+        if (randomValue<=2 && randomValue>11) {
+            System.out.println("Now is winter. It`s " + randomValue + " month");
         }
-        else {
-            String typeNumber="odd";
-            System.out.println("Number "+randomValue+" is "+typeNumber);
+        if (randomValue>2 && randomValue<=5) {
+            System.out.println("Now is spring. It`s " + randomValue + " month");
+        }
+        if (randomValue>5 && randomValue<=8) {
+            System.out.println("Now is summer. It`s "+randomValue+" month");
+        }
+        if (randomValue>8 && randomValue<=11) {
+            System.out.println("Now is autumn. It`s "+randomValue+" month");
         }
     }
 }
