d4a4e446511e537706e061fa805e73b88a31d4ce
dilo1992 <dimonchikov1992@gmail.com>
2022-08-10 20:13:00 +0300

    Classwork in a lesson 5

---
+++ src/Deep.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Deep {
+
+        public static void main(String[] args) {
+            int[][]arr=new int[4][];
+            arr[0]=new int[1];
+            arr[1]=new int[2];
+            arr[2]=new int[3];
+            arr[3]=new int[4];
+            for(int i = 0; i<arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    arr[i][j] = j;
+                }
+            }
+                System.out.println(Arrays.deepToString(arr));
+        //int a=0;
+        //int[][] arr2 = {{0},{0,1},{0,1,2},{0,1,2,3}};
+        //   System.out.println(Arrays.deepToString(arr2));
+
+        }
+}
