8de6c9226fe0c7ca1f9d4ef2c96214d79031712e
dilo1992 <dimonchikov1992@gmail.com>
2022-08-13 00:32:21 +0300

    Homework for a lesson 4, exercise 0

--- src/SearchNumber.java
+++ src/SearchNumber.java
@@ -8,15 +8,21 @@ public class SearchNumber {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your number (0...10): ");
         int number = scanner.nextInt();
-        if (number<0 || number>10){
-            System.out.println("Number does not match search parameters"); return;
-        } for (int i = 0; i < arr.length; i++) {
-            arr[i] = new Random().nextInt(0, 10);
-            System.out.println(arr[i]);
-        } if (Arrays.binarySearch(arr, number)>0) {
-            System.out.println("The entered number is present in the array under " + Arrays.binarySearch(arr, number) + " index");
-        } else {
-            System.out.println("The entered number is not in the array");
-            }
+        if (number < 0 || number > 10) {
+            System.out.println("Number does not match search parameters");
+            return;
         }
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new Random().nextInt(0, 10);
+            if (arr[i] == number) {
+                j = j + 1;
+            }
+        }   if (j>0){
+                System.out.print("The entered number is present in the array "+j+" once.");
+            } else if (j==0){
+                System.out.println("The entered number is not in the array");
+            }
+        System.out.println(Arrays.toString(arr));
     }
+}
