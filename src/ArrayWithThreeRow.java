b28bfad96313c0f683dc638f972920b71e930286
dilo1992 <dimonchikov1992@gmail.com>
2022-08-14 23:53:50 +0300

    Homework for a lesson 5, exercise 1

--- src/ArrayWithThreeRow.java
+++ src/ArrayWithThreeRow.java
@@ -4,23 +4,22 @@ import java.util.Scanner;
 
 public class ArrayWithThreeRow {
     public static void main(String[] args) {
-        int [][][] arr= new int [8][8][8];
-        Scanner scanner=new Scanner(System.in);
+        int[][][] arr = new int[8][8][8];
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number for summ: ");
-        int summnumb=scanner.nextInt();
-        //int elem=0;
-        for (int i=0;i< arr.length; i++){
-            for (int j=0;j<arr.length;j++){
-                for (int k=0;k< arr.length;k++){
-                    arr[i][j][k]=new Random().nextInt();
-                    }
-                }
-            }
-        System.out.println(Arrays.deepToString(arr));
-        for (int i=0;i< arr.length; i++) {
+        int summNumb = scanner.nextInt();
+        for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
                 for (int k = 0; k < arr.length; k++) {
-                    arr[i][j][k] += summnumb;
+                    arr[i][j][k] = new Random().nextInt();
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                for (int k = 0; k < arr.length; k++) {
+                    arr[i][j][k] += summNumb;
                 }
             }
         }
