551ed64793685261cc45f62230ae53551bd2337e
dilo1992 <dimonchikov1992@gmail.com>
2022-08-14 23:56:26 +0300

    Homework for a lesson 5, exercise 3

--- src/MatrixMultiplication.java
+++ src/MatrixMultiplication.java
@@ -9,17 +9,17 @@ public class MatrixMultiplication {
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr1.length; j++) {
                 arr1[i][j] = new Random().nextInt(0, 10);
-                arr2[i][j] = new Random().nextInt(0,10);
+                arr2[i][j] = new Random().nextInt(0, 10);
             }
             System.out.println(Arrays.toString(arr1[i]) + "            " + Arrays.toString(arr2[i]));
         }
-        int[][] arrfinal = new int[3][3];
-        System.out.println("arrfinal: ");
-        for (int i = 0; i < arrfinal.length; i++) {
-            for (int j = 0; j < arrfinal.length; j++) {
-                arrfinal[i][j] = arr1[i][j] * arr2[i][j];
+        int[][] arrFinal = new int[3][3];
+        System.out.println("arrFinal: ");
+        for (int i = 0; i < arrFinal.length; i++) {
+            for (int j = 0; j < arrFinal.length; j++) {
+                arrFinal[i][j] = arr1[i][j] * arr2[i][j];
             }
-            System.out.println(Arrays.toString(arrfinal[i]));
+            System.out.println(Arrays.toString(arrFinal[i]));
         }
     }
 }
