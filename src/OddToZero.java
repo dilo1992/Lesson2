8fc0bbcc29d866a36572982ffb9f5c3fe6574657
dilo1992 <dimonchikov1992@gmail.com>
2022-08-14 23:38:02 +0300

    Homework for a lesson 4, exercise 5

--- src/OddToZero.java
+++ src/OddToZero.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 
 public class OddToZero {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the size of a array: ");
-        int arrsize= scanner.nextInt();
-        int[] arr=new int[arrsize];
-        for(int i=0;i<arr.length;i++){
-            arr[i]=new Random().nextInt(0,15);
+        int arrSize = scanner.nextInt();
+        int[] arr = new int[arrSize];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new Random().nextInt(15);
         }
-        System.out.println("The array is "+ Arrays.toString(arr));
-        for (int i=0;i<arr.length;i++){
-            if (i%2!=0) {
+        System.out.println("The array is " + Arrays.toString(arr));
+        //for (int i=0;i<arr.length;i+=2){ //можно было использовать так и без if
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 != 0) {
                 arr[i] = 0;
             }
         }
-        System.out.println("The array without add numbers: "+Arrays.toString(arr));
-        }
+        System.out.println("The array without odd numbers: " + Arrays.toString(arr));
     }
+}
