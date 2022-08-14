9dfc3c1c7f9cd061326507d0524486946856f4aa
dilo1992 <dimonchikov1992@gmail.com>
2022-08-14 11:38:30 +0300

    Homework for a lesson 5, exercise 1

---
+++ src/ArrayWithThreeRow.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayWithThreeRow {
+    public static void main(String[] args) {
+        int [][][] arr= new int [8][8][8];
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter the number for summ: ");
+        int summnumb=scanner.nextInt();
+        //int elem=0;
+        for (int i=0;i< arr.length; i++){
+            for (int j=0;j<arr.length;j++){
+                for (int k=0;k< arr.length;k++){
+                    arr[i][j][k]=new Random().nextInt();
+                    }
+                }
+            }
+        System.out.println(Arrays.deepToString(arr));
+        for (int i=0;i< arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                for (int k = 0; k < arr.length; k++) {
+                    arr[i][j][k] += summnumb;
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(arr));
+    }
+}
