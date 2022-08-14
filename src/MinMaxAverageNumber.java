462ddb77d3db5cf9b83b61a85659b5910e3a0e71
dilo1992 <dimonchikov1992@gmail.com>
2022-08-14 23:32:30 +0300

    Homework for a lesson 4, exercise 2

--- src/MinMaxAverageNumber.java
+++ src/MinMaxAverageNumber.java
@@ -5,33 +5,32 @@ import java.util.Scanner;
 public class MinMaxAverageNumber {
     public static void main(String[] args) {
         System.out.print("Enter an array size: ");
-        Scanner scanner=new Scanner(System.in);
-        int arrsize=scanner.nextInt();
-        int[] arr=new int[arrsize];
-        for(int j=0;j<arr.length;j++){
-            arr[j]=new Random().nextInt(0,15);
+        Scanner scanner = new Scanner(System.in);
+        int arrSize = scanner.nextInt();
+        int[] arr = new int[arrSize];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new Random().nextInt(15);
         }
         System.out.println(Arrays.toString(arr));
-        int max=arr[0];
-        for (int k=1;k<arr.length;k++){
-            if (max<arr[k]){
-                max=arr[k];
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
             }
         }
-        System.out.println("The max number in an array is "+max);
-        int min=arr[0];
-        for (int k=1;k<arr.length;k++){
-            if (min>arr[k]){
-                min=arr[k];
+        System.out.println("The max number in an array is " + max);
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
             }
         }
-        System.out.println("The min number in an array is "+min);
-
-        int summ=0;
-        for (int k=0;k<arr.length;k++){
-            summ=summ+arr[k];
+        System.out.println("The min number in an array is " + min);
+        int summ = 0;
+        for (int i = 0; i < arr.length; i++) {
+            summ = summ + arr[i];
         }
-        float average= (float) summ/arrsize;
-        System.out.println("The average number in an array is "+average);
-        }
+        float average = (float) summ / arrSize;
+        System.out.println("The average number in an array is " + average);
     }
+}
