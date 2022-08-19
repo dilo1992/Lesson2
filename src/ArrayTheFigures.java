50868c27b70d735d6ed0fc9bdc95b370fbc8cd05
dilo1992 <dimonchikov1992@gmail.com>
2022-08-19 23:54:18 +0300

    Homework from the lesson 7,exercise 1

--- src/ArrayTheFigures.java
+++ src/ArrayTheFigures.java
@@ -1,6 +1,4 @@
-import java.util.Random;
-
-public class TestClass {
+public class ArrayTheFigures {
     public static void main(String[] args) {
         Figure circle = new Circle(3);
         Figure square = new Square(3);
