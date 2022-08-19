6ceb52637409a8f62d0ca5aadeaebf9edf13969b
dilo1992 <dimonchikov1992@gmail.com>
2022-08-20 00:58:04 +0300

    Homework from the lesson 7,exercise 2

---
+++ src/Director.java
@@ -0,0 +1,9 @@
+public class Director implements Workable {
+    public static void main(String[] args) {
+        @Override
+        public void work() {
+            System.out.println("This is Director");
+            ;
+        }
+    }
+}
---
+++ src/Workable.java
@@ -0,0 +1,5 @@
+public interface Workable {
+
+    public void work() {
+    }
+}
