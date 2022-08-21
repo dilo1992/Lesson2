41bab845a4c3eecb47aa7da6fbb6293805c614b0
dilo1992 <dimonchikov1992@gmail.com>
2022-08-21 23:53:02 +0300

    Homework from the lesson 6,exercise 3

--- src/ExerciseThree.java
+++ src/ExerciseThree.java
@@ -13,18 +13,51 @@ public class ExerciseThree {
         int sum = twenty * 20 + fifty * 50 + oneHundred * 100;
         Bankomat bankomat = new Bankomat(twenty, fifty, oneHundred);
         System.out.print("Enter remove sum: ");
-        int removesum= scanner.nextInt();
-        if (sum>=removesum){
-            int j=0;
+        int removeSum = scanner.nextInt();
+        if (removeSum != 20 & removeSum < 40 & removeSum % 10 != 0 & removeSum > sum) {
             do {
-                int i=removesum-100;
-                j++;
-            } while(removesum>=100 && j<oneHundred);
+                System.out.print("This operation isn`t done!");
+                break;
+            } while (true);
+        } else {
+            int removetwenty;
+            int removefifty;
+            int removeOneHundred;
+            if (removeSum == 20) {
+                removetwenty = removeSum / 20;
+            } else if (removeSum >= 40) {
+                int ostatok;
+                do {
+                    removeOneHundred = removeSum / 100;
+                    ostatok = removeSum % 100;
+                } while (ostatok > 110);
+                do {
+                    removefifty = ostatok / 50;
+                    ostatok %= 50;
+                } while (ostatok > 60);
+                removetwenty = ostatok / 20;
+                if (ostatok % 20==0) {
+                    System.out.println("You took " + removeSum + "$: " + removeOneHundred +
+                            " denomination banknots 100$, " + removefifty +
+                            " denomination banknots 50$, " + removetwenty +
+                            " denomination banknots 20$");
+                } else {
+                    System.out.println("You can`t withdraw money!");
+                }
+
+//        }
+    }
+//        if(sum >=removesum)
+//
+//    {
+//        int j = 0;
+//        do {
+//            int i = removesum - 100;
+//            j++;
+//        } while (removesum >= 100 && j < oneHundred);
 
             }
         }
-
-
 
 
     }
