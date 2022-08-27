3949650c83a662b9132cf44dd083eaffecf07164
Dmitry <102879601+dilo1992@users.noreply.github.com>
2022-08-27 23:59:46 +0300

    Delete Contract.java

--- src/by/teachmeskills/homework/lesson7/exercise3/Contract.java
+++
@@ -1,8 +0,0 @@
-package by.teachmeskills.homework.lesson7;
-
-public class Contract extends Document {
-    @Override
-    public void print() {
-        System.out.println("This is contract");
-    }
-}
