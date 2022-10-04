60653f51ee164ea1bf5d6a6fbd76855862400bef
dilo1992 <dimonchikov1992@gmail.com>
2022-10-04 22:50:31 +0300

    Homework for a lesson 17 (doesn`t filter file mix)

--- src/by/teachmeskills/homework/lesson17/DocumentsMain.java
+++ src/by/teachmeskills/homework/lesson17/DocumentsMain.java
@@ -7,31 +7,30 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+
 public class DocumentsMain {
-    public static void main(String[] args) {//throws IOException, RuntimeException, NotFoundException {
+    public static void main(String[] args) throws IOException, RuntimeException, NotFoundException {
         Map<String, Document> mapOfDocuments = new TreeMap<>();
-        System.out.println("Enter document folder name: ");
-        Scanner scanner = new Scanner(System.in);
-        String pathOfFolder = scanner.nextLine();
-        File file = new File(pathOfFolder);
-
-
-        try {
-            do {
+        File file;
+        do {
+            System.out.println("Enter document folder name: ");
+            Scanner scanner = new Scanner(System.in);
+            file = new File(scanner.nextLine());
+            Optional<String[]> optionalListOfFiles = Optional.ofNullable(file.list());
+            if (optionalListOfFiles.isEmpty()) {
+                System.out.println("This folder is empty");
+                continue;
+            } else {
                 List<String> arrOfFiles = List.of(file.list());
-                if (arrOfFiles.isEmpty()) {
-                    System.out.println("This folder is empty");
-                } else {
-                    Optional<Long> count = Optional.ofNullable(Optional.of(arrOfFiles.stream()
-                                    .filter(x -> x.endsWith(".txt"))
-                                    .count())
-                            .orElseThrow(() -> new NotFoundException("This folder does not contain files of the required format ")));
-                }
-            } while (true);
-        } catch (NullPointerException | NotFoundException | SecurityException e) {
-            System.out.println(e.getMessage());
-        }
-
+                long count = arrOfFiles.stream()
+                        .filter(x -> x.endsWith(".txt"))
+                        .count();
+                if(count == 0) {
+                    System.out.println("This folder does not contain files of the required format ");
+                } else break;
+            }
+        } while (true);
 
         String docNumberCheckerPattern = "(\\d{4}-)([a-zA-Z]{3}-)(\\d{4}-)([a-zA-Z]{3}-)(\\d[a-zA-Z]\\d[a-zA-Z])";
         String emailCheckerPattern = "[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}";
@@ -39,22 +38,15 @@ public class DocumentsMain {
         Pattern pattern = Pattern.compile(docNumberCheckerPattern);
         Pattern pattern1 = Pattern.compile(emailCheckerPattern, Pattern.CASE_INSENSITIVE);
         Pattern pattern2 = Pattern.compile(phoneNumberCheckerPattern);
-
-        try {
-            for (File currentFile : file.listFiles()) {
-                StringBuilder sb = getKeyForMapFromFileName(currentFile);
-
-                Document document = new Document();
-                List<String> numberDoc = new ArrayList<>();
-                getDocNumberForCycle(pattern, currentFile, numberDoc);
-                document.setDocumentsNumber(numberDoc);
-                getEmailForCycle(pattern1, currentFile, document);
-                getPhoneNumbForCycle(pattern2, currentFile, document);
-
-                mapOfDocuments.put(sb.toString(), document);
-            }
-        } catch (NullPointerException e) {
-            System.out.println(e.getMessage());
+        for (File currentFile : file.listFiles()) {
+            StringBuilder sb = getKeyForMapFromFileName(currentFile);
+            Document document = new Document();
+            List<String> numberDoc = new ArrayList<>();
+            getDocNumberForCycle(pattern, currentFile, numberDoc);
+            document.setDocumentsNumber(numberDoc);
+            getEmailForCycle(pattern1, currentFile, document);
+            getPhoneNumbForCycle(pattern2, currentFile, document);
+            mapOfDocuments.put(sb.toString(), document);
         }
         System.out.println(mapOfDocuments);
     }
