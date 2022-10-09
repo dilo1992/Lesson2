22895f517090aedb4b7f6e654bc069d6c8dbf2c7
dilo1992 <dimonchikov1992@gmail.com>
2022-10-10 00:08:57 +0300

    Homework for lessons 18

--- src/by/teachmeskills/homework/lesson18/SAXParserMain.java
+++ src/by/teachmeskills/homework/lesson18/SAXParserMain.java
@@ -1,12 +1,17 @@
 package by.teachmeskills.homework.lesson18;
 
+
 import by.teachmeskills.homework.lesson17.MyNameFileFilter;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
+import javax.xml.parsers.*;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -16,9 +21,39 @@ public class SAXParserMain {
     private static ArrayList<String> text = new ArrayList<>();
     private static File XMLForProcess;
 
-    public static void main(String[] args) throws ParserConfigurationException, SAXException {
-        File[] listFiles;
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
         File file;
+        int numberOfChoice;
+        file = setPathOfFolderWithParsingFile();
+
+        numberOfChoice = setParsingType();
+        if (numberOfChoice == 1) {
+            SAXMethod(file);
+        } else if (numberOfChoice == 2) {
+            DOMMethod(file);
+        }
+    }
+
+    private static int setParsingType() {
+        int numberOfChoice;
+        do {
+            System.out.println("Enter needed method of document processing:\n 1 - SAX\n 2 - DOM\n ");
+            Scanner scannerParseMethod = new Scanner(System.in);
+            Pattern pattern = Pattern.compile("^[1-2]$");
+            Matcher matcher = pattern.matcher(scannerParseMethod.nextLine());
+            if (matcher.find()) {
+                numberOfChoice = Integer.parseInt(matcher.group());
+                break;
+            } else {
+                System.out.println("Entered data does not match the requirements");
+            }
+        } while (true);
+        return numberOfChoice;
+    }
+
+    private static File setPathOfFolderWithParsingFile() {
+        File file;
+        File[] listFiles;
         do {
             System.out.println("Enter document folder name: ");
             Scanner scanner = new Scanner(System.in);
@@ -45,35 +80,45 @@ public class SAXParserMain {
                 break;
             }
         } while (true);
+        return file;
+    }
 
-        System.out.println(XMLForProcess.getName());
+    private static void DOMMethod(File file) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory DOMFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder DOMBuilder = DOMFactory.newDocumentBuilder();
+        Document document = DOMBuilder.parse(XMLForProcess);
+        NodeList nodeListLines = document.getElementsByTagName("lines");
+        NodeList nodeListLine = document.getElementsByTagName("line");
+        for (int i = 0; i < nodeListLine.getLength(); i++) {
+            Node current = nodeListLines.item(0);
+            Element element = (Element) current;
+            text.add(element.getElementsByTagName("line").item(i).getTextContent());
+        }
+        writeToFileAfterParse(file);
+    }
 
-//        int numberOfChoice;
-//        do {
-//            System.out.println("Enter needed method of document processing:\n 1 - SAX\n 2 - DOM\n ");
-//            Scanner scannerParseMethod = new Scanner(System.in);
-//            Pattern pattern = Pattern.compile("^[1-2]$");
-//            Matcher matcher = pattern.matcher(scannerParseMethod.nextLine());
-//            if (matcher.find()) {
-//                numberOfChoice = Integer.parseInt(matcher.group());
-//                break;
-//            } else {
-//                System.out.println("Entered data does not match the requirements");
-//            }
-//        } while (true);
-//
-////        if (numberOfChoice == 1) {
-////
-////        } else if (numberOfChoice == 2) {
-////
-////        }
-
+    private static void SAXMethod(File file) throws ParserConfigurationException, SAXException, IOException {
         SAXParserFactory factory = SAXParserFactory.newInstance();
         SAXParser parser = factory.newSAXParser();
+        XMLSAXHandler SAXHandler = new XMLSAXHandler();
+        parser.parse(XMLForProcess, SAXHandler);
+        text = SAXHandler.getListWithText();
 
-
-
-
-
+        writeToFileAfterParse(file);
     }
+
+    private static void writeToFileAfterParse(File file) {
+        String addName = "sonnet_lines_line.txt";
+        String path = file.getPath() + "\\" + addName;
+        try (FileWriter fileWriter = new FileWriter(path)) {
+            for (String string : text) {
+                fileWriter.write(string + "\n");
+            }
+            fileWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
