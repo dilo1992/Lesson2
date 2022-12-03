cbd7fcba4ed40d63e50cca6ec7a64259756c309f
dmitrylobov <dilo1992.edu@gmail.com>
2022-12-03 23:24:59 +0300

    Homework for a lesson 20 (corrected after teacher`s comments)

--- src/by/teachmeskills/homework/lesson20/JDBCStudents.java
+++ src/by/teachmeskills/homework/lesson20/JDBCStudents.java
@@ -12,21 +12,15 @@ public class JDBCStudents {
     public static final String URL = "jdbc:mysql://localhost:3306/students_db";
     private static String username;
     private static String password;
-    private static int countAddStudents;
-    private static int countAddCities;
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException, RuntimeException {
 
-        String choiceForPattern;
         int choice = 0;
-        ResultSet resultSet = null;
 
         System.out.print("Enter username for a database access: ");
-        Scanner scanner = new Scanner(System.in);
-        setUsername(scanner.nextLine());
+        setUsername(new Scanner(System.in).nextLine());
         System.out.print("Enter password for a database access: ");
-        Scanner scanner1 = new Scanner(System.in);
-        setPassword(scanner1.nextLine());
+        setPassword(new Scanner(System.in).nextLine());
         try {
             Class.forName(JDBC_DRIVER);
         } catch (ClassNotFoundException e) {
@@ -38,22 +32,17 @@ public class JDBCStudents {
 
             firstInitDatabase(statement);
 
-            do {
-                choice = setChoice(choice, connection, statement);
 
-                if (choice == 1) {
-                    resultSet = viewStudentsList(resultSet, statement);
-                } else if (choice == 2) {
-                    resultSet = addStudent(resultSet, statement);
-                } else if (choice == 3) {
-                    resultSet = addCity(resultSet, statement);
-                } else if (choice == 4) {
-                    resultSet = deleteStudent(resultSet, statement);
-                } else if (choice == 5) {
-                    resultSet = deleteCity(resultSet, statement);
-                } else if (choice == 6) {
-                    exitMethod(connection, statement);
-                    break;
+            do {
+                choice = getChoice(choice, connection);
+
+                switch (choice) {
+                    case 1 -> viewStudentsList(statement);
+                    case 2 -> addStudent(statement);
+                    case 3 -> addCity(statement);
+                    case 4 -> deleteStudent(statement);
+                    case 5 -> deleteCity(connection, statement);
+                    case 6 -> exitMethod(connection);
                 }
             } while (true);
         } catch (SQLException e) {
@@ -61,128 +50,68 @@ public class JDBCStudents {
         }
     }
 
-    private static ResultSet deleteCity(ResultSet resultSet, Statement statement) throws SQLException {
-        setCountAddCities(getCountAddCities() - 1);
-        System.out.println("Enter a city name to be deleted: ");
-        Scanner scannerDeleteCity = new Scanner(System.in);
-        String nameDeleteCity = scannerDeleteCity.nextLine();
-        System.out.println("Enter a number of student: ");
-        int numberOfStudentForDeleteCity = new Scanner(System.in).nextInt();
-        String commandDeleteCity = "delete from cities where id=" + numberOfStudentForDeleteCity;
-        statement.executeUpdate(commandDeleteCity);
-        if (getCountAddStudents() > getCountAddCities()) {
-            resultSet = statement.executeQuery("select students.id, students.name,cities.city " +
-                    "from students left join cities on students.id = cities.id");
-        } else if (getCountAddStudents() == getCountAddCities()) {
-            resultSet = statement.executeQuery("select students.id, students.name,cities.city " +
-                    "from students join cities on students.id = cities.id");
-        } else if (getCountAddStudents() < getCountAddCities()) {
-            resultSet = statement.executeQuery("select students.id, students.name,cities.city " +
-                    "from students right join cities on students.id = cities.id");
-        }
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("id") + "\t" + resultSet.getString("name") +
-                    "\t" + resultSet.getString("city"));
-        }
-        return resultSet;
+    private static void firstInitDatabase(Statement statement) throws SQLException {
+        statement.executeUpdate("create database if not exists students_db;");
+        statement.executeUpdate("use students_db;");
+        statement.executeUpdate("create table if not exists students (student_id integer primary key not null, city_id integer not null, name varchar(30) not null);");
+        statement.executeUpdate("create table if not exists cities (id integer primary key auto_increment not null, city_id integer not null, city varchar(30));");
     }
 
-    private static ResultSet deleteStudent(ResultSet resultSet, Statement statement) throws SQLException {
-        setCountAddStudents(getCountAddStudents() - 1);
-        System.out.println("Enter name a student to be deleted: ");
-        Scanner scannerDeleteStudent = new Scanner(System.in);
-        String nameDeleteStudent = scannerDeleteStudent.nextLine();
-        String commandDeleteStudent = "delete from students where name='" + nameDeleteStudent + "'";
-        statement.executeUpdate(commandDeleteStudent);
-        if (getCountAddStudents() > getCountAddCities()) {
-            resultSet = statement.executeQuery("select students.id, students.name,cities.city " +
-                    "from students left join cities on students.id = cities.id");
-        } else if (getCountAddStudents() == getCountAddCities()) {
-            resultSet = statement.executeQuery("select students.id, students.name,cities.city " +
-                    "from students join cities on students.id = cities.id");
-        } else if (getCountAddStudents() < getCountAddCities()) {
-            resultSet = statement.executeQuery("select students.id, students.name,cities.city " +
-                    "from students right join cities on students.id = cities.id");
-        }
+    private static void viewStudentsList(Statement statement) throws SQLException {
+        ResultSet resultSet = statement.executeQuery("select students.student_id,students.name,cities.city " +
+                "from students left join cities on students.city_id = cities.city_id;");
         while (resultSet.next()) {
-            System.out.println(resultSet.getString("id") + "\t" + resultSet.getString("name") +
+            System.out.println(resultSet.getString("student_id") + "\t" + resultSet.getString("name") +
                     "\t" + resultSet.getString("city"));
         }
-        return resultSet;
     }
 
-    private static ResultSet addCity(ResultSet resultSet, Statement statement) throws SQLException {
-        setCountAddCities(getCountAddCities() + 1);
-        System.out.println("Enter a new city name: ");
-        String nameNewCity = new Scanner(System.in).nextLine();
-        System.out.println("Enter a number of student: ");
-        int numberOfStudentForAddCity = new Scanner(System.in).nextInt();
-        String commandAddCity = "insert into cities (id,city) values (" + numberOfStudentForAddCity + ",'" + nameNewCity + "')";
-        statement.executeUpdate(commandAddCity);
-        if (getCountAddStudents() > getCountAddCities()) {
-            resultSet = statement.executeQuery("select students.id, students.name,cities.city " +
-                    "from students left join cities on students.id = cities.id");
-        } else if (getCountAddStudents() == getCountAddCities()) {
-            resultSet = statement.executeQuery("select students.id, students.name,cities.city " +
-                    "from students join cities on students.id = cities.id");
-        } else if (getCountAddStudents() < getCountAddCities()) {
-            resultSet = statement.executeQuery("select students.id, students.name,cities.city " +
-                    "from cities left join students on students.id = cities.id");
-        }
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("id") + "\t" + resultSet.getString("name") +
-                    "\t" + resultSet.getString("city"));
-        }
-        return resultSet;
-    }
-
-    private static ResultSet addStudent(ResultSet resultSet, Statement statement) throws SQLException {
-        setCountAddStudents(getCountAddStudents() + 1);
-        System.out.println("Enter name a new student: ");
+    private static void addStudent(Statement statement) throws SQLException {
+        System.out.println("Enter new student id: ");
+        int idNewStudent = new Scanner(System.in).nextInt();
+        System.out.println("Enter the name of the new student: ");
         String nameNewStudent = new Scanner(System.in).nextLine();
-        String commandAddStudent = "insert into students (name) values ('" + nameNewStudent + "')";
+        System.out.println("Enter city id for the new student: ");
+        int cityIdNewStudent = new Scanner(System.in).nextInt();
+        String commandAddStudent = "insert into students (student_id,city_id, name) values (" + idNewStudent + ","
+                + cityIdNewStudent + ",'" + nameNewStudent + "');";
         statement.executeUpdate(commandAddStudent);
-        if (getCountAddStudents() > getCountAddCities()) {
-            resultSet = statement.executeQuery("select students.id, students.name,cities.city " +
-                    "from students left join cities on students.id = cities.id");
-        } else if (getCountAddStudents() == getCountAddCities()) {
-            resultSet = statement.executeQuery("select students.id, students.name,cities.city " +
-                    "from students join cities on students.id = cities.id");
-        } else if (getCountAddStudents() < getCountAddCities()) {
-            resultSet = statement.executeQuery("select students.id, students.name,cities.city " +
-                    "from students right join cities on students.id = cities.id");
-        }
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("id") + "\t" + resultSet.getString("name") +
-                    "\t" + resultSet.getString("city"));
-        }
-        return resultSet;
+        viewStudentsList(statement);
     }
 
-    private static ResultSet viewStudentsList(ResultSet resultSet, Statement statement) throws SQLException {
-        if (getCountAddStudents() > getCountAddCities()) {
-            resultSet = statement.executeQuery("select students.id,students.name,cities.city " +
-                    "from students left join cities on students.id = cities.id");
-        } else if (getCountAddStudents() == getCountAddCities()) {
-            resultSet = statement.executeQuery("select students.id,students.name,cities.city " +
-                    "from students join cities on students.id = cities.id");
-        } else if (getCountAddStudents() < getCountAddCities()) {
-            resultSet = statement.executeQuery("select students.id,students.name,cities.city " +
-                    "from students right join cities on students.id = cities.id");
-        }
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("id") + "\t" + resultSet.getString("name") +
-                    "\t" + resultSet.getString("city"));
-        }
-        return resultSet;
+    private static void addCity(Statement statement) throws SQLException {
+        System.out.println("Enter new city id: ");
+        int idNewCity = new Scanner(System.in).nextInt();
+        System.out.println("Enter the name of the new city: ");
+        String nameNewCity = new Scanner(System.in).nextLine();
+        String commandAddCity = "insert into cities (city_id,city) values (" + idNewCity + ",'" + nameNewCity + "');";
+        statement.executeUpdate(commandAddCity);
+        viewStudentsList(statement);
     }
 
-    private static int setChoice(int choice, Connection connection, Statement statement) throws SQLException {
-        String choiceForPattern;
+    private static void deleteStudent(Statement statement) throws SQLException {
+        System.out.println("Enter a student id to be deleted: ");
+        int studentIdForDeleteStudent = new Scanner(System.in).nextInt();
+        String commandDeleteStudent = "delete from students where student_id=" + studentIdForDeleteStudent + ";";
+        statement.executeUpdate(commandDeleteStudent);
+        viewStudentsList(statement);
+    }
+
+    private static void deleteCity(Connection connection, Statement statement) throws SQLException {
+        System.out.println("Enter the city id to be deleted: ");
+        String cityIdForDeleteCity = new Scanner(System.in).next();
+        String firstCommandDeleteCity = "update cities set city=null where city_id=" + cityIdForDeleteCity;
+        String secondCommandDeleteCity = "delete from cities where cities.city=null";
+        connection.setAutoCommit(false);
+        statement.executeUpdate(firstCommandDeleteCity);
+        statement.executeUpdate(secondCommandDeleteCity);
+        connection.commit();
+        viewStudentsList(statement);
+    }
+
+    private static int getChoice(int choice, Connection connection) throws SQLException {
         System.out.println("Enter what you need now: \n1. View students list\n2. Add student\n3. Add city\n4. Delete student\n5. Delete city\n6. Exit");
-        Scanner scanner2 = new Scanner(System.in);
-        choiceForPattern = scanner2.nextLine();
-
+        String choiceForPattern = new Scanner(System.in).nextLine();
 
         Pattern patternForChoice = Pattern.compile("^\\d+$");
         Matcher matcherForChoice = patternForChoice.matcher(choiceForPattern);
@@ -191,31 +120,19 @@ public class JDBCStudents {
             choice = parseInt(choiceForPattern);
             if (choice < 1 && choice > 6) {
                 System.out.println("invalid input");
-                exitMethod(connection, statement);
+                exitMethod(connection);
             }
         } else {
             System.out.println("invalid input");
-            exitMethod(connection, statement);
+            exitMethod(connection);
         }
         return choice;
     }
 
-    private static void firstInitDatabase(Statement statement) throws SQLException {
-        statement.executeUpdate("create database if not exists students_db");
-        statement.executeUpdate("use students_db");
-        statement.executeUpdate("drop table students");
-        statement.executeUpdate("drop table cities");
-        statement.executeUpdate("create table if not exists students (id integer primary key auto_increment not null, name varchar(30) not null)");
-        statement.executeUpdate("create table if not exists cities (id integer primary key not null, city varchar(30) not null)");
-        statement.executeUpdate("insert into students (name) values ('Vasily'), ('Dmitry'), ('Sergey'), ('Ivan'), ('Igor'), ('Gennady'), ('Anton')");
-        statement.executeUpdate("insert into cities (id, city) values (1,'Brest'), (2,'Grodno'), (3,'Brest'), (4,'Minsk'), (5,'Smolevichi'), (6,'Minsk'), (7,'Vitebsk')");
-    }
-
-    private static void exitMethod(Connection connection, Statement statement) throws SQLException {
-        statement.executeUpdate("drop table students");
-        statement.executeUpdate("drop table cities");
+    private static void exitMethod(Connection connection) throws SQLException {
         connection.close();
     }
+
 
     public static void setUsername(String username) {
         JDBCStudents.username = username;
@@ -231,21 +148,5 @@ public class JDBCStudents {
 
     public static String getPassword() {
         return password;
-    }
-
-    public static int getCountAddStudents() {
-        return countAddStudents;
-    }
-
-    public static void setCountAddStudents(int countAddStudents) {
-        JDBCStudents.countAddStudents = countAddStudents;
-    }
-
-    public static int getCountAddCities() {
-        return countAddCities;
-    }
-
-    public static void setCountAddCities(int countAddCities) {
-        JDBCStudents.countAddCities = countAddCities;
     }
 }
